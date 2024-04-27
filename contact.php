441283645fcc2d269ae0dc8e4b4bf931cc55cb37
Manna97 <juanmapra@yahoo.com.ar>
2024-04-27 10:54:12 -0300

    Creada db y conexion a db

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 <section class="contact-form" style="display: flex; align-content:center; flex-flow:column wrap;">
         <h2> Contacto</h2>
-        <form action="" method="post">
+        <form action="enviar_consulta.php" method="post">
                 <div style="display:flex;flex-flow:row wrap;justify-content: space-between;">
                     <p style="display: flex;flex-flow: column;flex-grow:1;margin-right:5px;">
                         <label for="nombre">Nombre</label>
@@ -18,5 +18,7 @@
             <input class="submit-btn" type="submit" value="Enviar consulta" style="font-size: 1.5rem;background-color:transparent;color:inherit;">
         </form>
 </section>
-
+<?php if(isset($_GET['ok'])){
+    echo "<h3>Mensaje enviado correctamente.</h3>";
+} ?>
 <?php include("footer.php") ?>
--- enviar_consulta.php
+++ enviar_consulta.php
@@ -16,9 +16,23 @@ $cabeceras = 'From: ' . $correo_form . "\r\n" .
             'Reply-To: ' . $correo_form . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
+mail($remitente, $asunto, $cuerpo_mail, $cabeceras);
+
 if (mail($remitente, $asunto, $cuerpo_mail, $cabeceras)){
     echo "<p>El mensaje se ha enviado correctamente.</p>";
 } else {
     echo "<p>Error al enviar el mensaje.</p>";
 }
+
+// $connection = mysqli_connect('localhost', 'root', '', 'desarrollo_php_utn') or die ("No se pudo conectar a la base de datos");
+
+$connection = mysqli_connect('sql110.infinityfree.com', 'if0_36262667', 'wCQ5SCdoK789Ea', 'if0_36262667_desarrollo_php_utn') or die ("No se pudo conectar a la base de datos");
+
+$query = "INSERT INTO consulta_form VALUES (DEFAULT, '$nombre_form', '$correo_form', '$mensaje_form')";
+
+mysqli_query($connection, $query);
+
+header("Location: contact.php?ok");
+mysqli_close($connection);
+exit;
 ?>
